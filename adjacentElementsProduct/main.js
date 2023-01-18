9b99234d463ad0b93908afc27521816c388f855f
Eriberto-SG <eribertosg.dev@gmail.com>
2023-01-18 02:44:29 -0600

    Se creo el algoritmo para encontrar el par mas grande

--- adjacentElementsProduct/main.js
+++ adjacentElementsProduct/main.js
@@ -1,3 +1,15 @@
 //Dada una matriz de enteros, encuentra el par de elementos adyacentes 
 //que tiene el producto más grande y devuelve ese producto.
 
+function findLargestAdjacentProduct(arr) {
+  let largestProduct = -Infinity
+  for (let i = 0; i < arr.length - 1; i++) {
+    let product = arr[i] * arr[i + 1]
+    if (product > largestProduct) {
+      largestProduct = product
+    }
+  }
+  return largestProduct
+}
+
+console.log(findLargestAdjacentProduct([1, 2]))
